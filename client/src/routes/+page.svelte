abaa959b83fe2750ef7ffa7a2e429c86f9a2505e
Marco Tidu <tidumarco@gmail.com>
2024-02-27 15:07:03 +0100

    Refreshed app

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Title placeholder</h1>
+</section>
+
+<style>
+@import "../styles/page.css";
+</style>
