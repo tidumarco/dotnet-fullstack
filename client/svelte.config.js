262c53878fbabf36cf17f9e83cd1d5848e79695b
Marco Tidu <tidumarco@gmail.com>
2024-03-01 22:56:17 +0100

    Commit made from VS Code Azure Static Web Apps

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import azure from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +11,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: azure()
+		// adapter: adapter()
 		// csrf: {
 		// 	checkOrigin: false
 		// }
