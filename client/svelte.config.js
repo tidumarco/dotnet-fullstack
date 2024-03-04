eb7d55526f14e24a9f7d1f5db3c18f2acfdd3b29
Marco Tidu <tidumarco@gmail.com>
2024-03-04 10:40:44 +0100

    testing renaming file

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,3 @@
-// import azure from 'svelte-adapter-azure-swa';
 import adapter from '@sveltejs/adapter-auto';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
@@ -13,7 +12,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-		// adapter: azure()
 	}
 };
 
